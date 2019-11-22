b37c1f239a523fd0249569e96532fea04b78e89b
Wikkyx <57778588+Wikkyx@users.noreply.github.com>
2019-11-21 19:46:36 -0600

    Update Trivia.java

--- src/src/Trivia.java
+++ src/src/Trivia.java
@@ -1,16 +1,29 @@
 package src;
 
+import java.util.Scanner;
+
 public class Trivia {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int opc;
 		char a='s';
 		
+		//Crear objetos de clases
+		
+		Scanner sc = new Scanner (System.in);
+		
 		while(a=='s') {
-		System.out.printf ("\n      Juego de Trivia      ");
-		System.out.printf ("\n1.  Configuracion");
-		System.out.printf ("\n2.  Juego");
-		System.out.printf ("\n3.  Consulta de puntaje obtenido");
-		System.out.printf ("\n4.  Salir");
+		System.out.printf ("\n      	Juego de Trivia      ");
+		System.out.printf ("\n[1].  Configuracion");
+		System.out.printf ("\n[2].  Juego");
+		System.out.printf ("\n[3].  Consulta de puntaje obtenido");
+		System.out.printf ("\n[4].  Salir");
+		
+		opc = sc.nextInt();
+		switch (opc) {
+			case 1:
+				
+		
 		}
 	     
 		
@@ -22,4 +35,6 @@ public class Trivia {
 	
 	
 	}
+}
+
 }
