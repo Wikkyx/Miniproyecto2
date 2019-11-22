8b966aa9f06a37f3387ebfc4e7284f3f285170d3
Jngamero <57850554+Jngamero@users.noreply.github.com>
2019-11-21 19:35:54 -0500

    Create Trivia.java

---
+++ src/src/Trivia.java
@@ -0,0 +1,25 @@
+package src;
+
+public class Trivia {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char a='s';
+		
+		while(a=='s') {
+		System.out.printf ("\n      Juego de Trivia      ");
+		System.out.printf ("\n1.  Configuracion");
+		System.out.printf ("\n2.  Juego");
+		System.out.printf ("\n3.  Consulta de puntaje obtenido");
+		System.out.printf ("\n4.  Salir");
+		}
+	     
+		
+
+
+		
+	
+	
+	
+	
+	}
+}
