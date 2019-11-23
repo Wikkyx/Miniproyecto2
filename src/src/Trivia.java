a8384299e29f397e4a809403df3c50d95850b5c5
Jngamero <57850554+Jngamero@users.noreply.github.com>
2019-11-22 22:49:11 -0500

    Update Trivia.java

--- src/src/Trivia.java
+++ src/src/Trivia.java
@@ -6,6 +6,7 @@ public class Trivia {
 		public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int opc,change;
+		String resp;
 		char a='s';
 		String pregunta_config;
 		String preguntas_posibles[] = new String [4];
@@ -57,7 +58,6 @@ public class Trivia {
 		change = sc.nextInt();
 		switch(change) {
 		case 1: 
-			String resp;
 			System.out.printf ("\n1. ¿Año en que se descubre America? "); 
 			System.out.printf ("\n a) 1492 "); 
 			System.out.printf ("\n b) 1512 "); 
@@ -66,13 +66,90 @@ public class Trivia {
 			System.out.printf ("\n Seleccione la respuesta correcta"); 
 			resp=sc.next();
 			System.out.printf ("\n Seleccione la respuesta correcta"); 
-
+			
+			
+		case 2:
+			
+			System.out.printf ("\n1. Cúal es la cabecera de la provincia de Coclé?"); 
+			System.out.printf ("\n Aguadulce  "); 
+			System.out.printf ("\n Penonomé  "); 
+			System.out.printf ("\n Anton "); 
+			System.out.printf ("\n Nata "); 
+			System.out.printf ("\nSeleccione la respuesta correcta "); 
+		    resp=sc.next();
+			System.out.printf ("\n Seleccione la respuesta correcta "); 
 		
+		case 3: 
+			System.out.printf ("\n3. ¿Cual es raiz cuadrada de 3600? "); 
+			System.out.printf ("\n  600"); 
+			System.out.printf ("\n 6 "); 
+			System.out.printf ("\n 60 "); 
+			System.out.printf ("\n 6000 "); 
+			System.out.printf ("\nSeleccione la respuesta correcta "); 
+			resp=sc.next();
+			
+		case 4: 
+			System.out.printf ("\n4. Panamá limita al norte con "); 
+			System.out.printf ("\n Panamá limita al norte con "); 
+			System.out.printf ("\n Panamá limita al norte con "); 
+			System.out.printf ("\n Panamá limita al norte con "); 
+			System.out.printf ("\n Panamá limita al norte con "); 
+			System.out.printf ("\nSeleccione la respuesta correcta ");
+			resp=sc.next();
 		
+		case 5:
+			System.out.printf ("\n5. ¿Cantidad de Comarcas indigenas que tiene Panamá? "); 
+			System.out.printf ("\n 1 "); 
+			System.out.printf ("\n 5 "); 
+			System.out.printf ("\n 3 "); 
+			System.out.printf ("\n 6 "); 
+			System.out.printf ("\nSeleccione la respuesta correcta "); 
+			resp=sc.next();
+			
+		case 6:
+			System.out.printf ("\n ¿Cuantos distritos tiene la provincia? "); 
+			System.out.printf ("\n 78 "); 
+			System.out.printf ("\n58 "); 
+			System.out.printf ("\n 68"); 
+			System.out.printf ("\n 48 "); 
+			System.out.printf ("\nSeleccione la respuesta correcta "); 
+			resp=sc.next();
+			
+		case 7:
+			System.out.printf ("\n7.  Panamá limita con Colombia al"); 
+			System.out.printf ("\n Norte"); 
+			System.out.printf ("\n Sur"); 
+			System.out.printf ("\n  Este"); 
+			System.out.printf ("\n Oeste"); 
+			System.out.printf ("\nSeleccione la respuesta correcta "); 
+			resp=sc.next();
 		
-		//sdfasdfsdfsdaf
+		case 8:
+			System.out.printf ("\n8.  El itsmo de Panamá fue visitado por primera vez por  "); 
+			System.out.printf ("\n Vasco Núñez de Balboa"); 
+			System.out.printf ("\n Rodrigo de Bastidas  "); 
+			System.out.printf ("\n  Diego de nicueza  "); 
+			System.out.printf ("\n Cristobal Colón  "); 
+			System.out.printf ("\nSeleccione la respuesta correcta "); 
+			resp=sc.next();
 		
+		case 9:
+			System.out.printf("\n9. ¿Año en que fue fundado la ciudad de Panamá?");
+			System.out.printf("\n 1519");
+			System.out.printf("\n 1529");
+			System.out.printf("\n 1512");
+			System.out.printf("\n 1509");
+			System.out.printf ("\nSeleccione la respuesta correcta "); 
+			resp=sc.next();
 		
+		case 10:
+			System.out.printf("\n10. ¿Fue el primer europeo en division el oceano Pacifico?");
+			System.out.printf("\n Cristobal Colón");
+			System.out.printf("\n Vazvo Nuñez de Balboa ");
+			System.out.printf("\n Rodrigo de Bastidas");
+			System.out.printf("\nDiego de Nicueza");
+			System.out.printf ("\nSeleccione la respuesta correcta "); 
+			resp=sc.next();
 		}
 	}
 }
